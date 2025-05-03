8fd17eb5d7fd5d82bdaf1980167e87b2f0075908
Deepesh <deepesh@osiansoftware.com>
2025-05-03 19:24:33 +0530

    added secret data in env variables

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NmM3MjU4NTQxM2VlNjY1NWYyNWViYjBiNmI2YzkxNSIsIm5iZiI6MTc0MzIzODQ4OS4yNjksInN1YiI6IjY3ZTdiNTU5NmIzNjdkNDY5NTY3YjIxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vfr4reBLDPg6SVyRmC6Ybgz_Kfty5VO-WJrSGHNYelU'
+        Authorization: 'Bearer ' + import.meta.env.VITE_TMDB_AUTH_KEY
     }
 };
 
@@ -24,5 +24,5 @@ export const languageOptions = [
         name: 'Spanish'
     }
 ]
-export const GENAI_KEY = 'AIzaSyAfevheXquisw4wX5aCA_n-gF6Wkl9x9d8'
+export const GENAI_KEY = import.meta.env.VITE_GENAI_KEY
 export const improvisedPrompt = `act like a movie recommendation system and according to the given text evaluate the movies and give response for 5 movies in array format like this, ['movieName1','movieName2',movieName3'...], only give array in response and no other description strictly only array like this ['movieName1','movieName2',movieName3','movieName4','movieName5']. text - `
